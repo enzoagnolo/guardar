8e98aa0f9df477725fda36963e004b833a601000
enzoagnolo <158178827+enzoagnolo@users.noreply.github.com>
2026-04-23 11:11:38 -0300

    Add files via upload

---
+++ RefacçãoProvas/DIA 2/java.js
@@ -0,0 +1,27 @@
+function revelar() {
+
+    let img = document.querySelector(".card img");
+    img.src = "_vinicius_junior.png";
+
+    let nome = document.getElementById("Nome");
+    let rank = document.getElementById("Rank");
+    let data = document.getElementById("Data_Nas");
+    let altura = document.getElementById("Alutra");
+    let posicao = document.querySelector('[id="Posição "]');
+
+    nome.innerHTML = "Vinícius José Paixão de Oliveira Júnior ";
+    nome.appendChild(rank); 
+
+    rank.innerText = "9,5";
+
+    data.innerText = "📅 12/07/2000 (25 anos)";
+    altura.innerText = "📏 1,76 m";
+    posicao.innerText = "🏃 Ponta-esquerda / Atacante";
+
+    let elementos = [nome, data, altura, posicao];
+
+    elementos.forEach(el => {
+        el.classList.remove("placeholder", "placeholder-glow");
+        el.classList.add("card-text");
+    });
+}
